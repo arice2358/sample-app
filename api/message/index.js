596db94477d9d156e588faf7d41152ddd710b652
Adam <adamrice@outlook.com>
2023-07-10 21:36:08 +0100

    changes

--- api/message/index.js
+++ api/message/index.js
@@ -1,19 +1,28 @@
 const customers = require('../data/customers.json');
 
 module.exports = async function (context, req) {
+    context.log('Header: ' + req.headers['authorization']);
+
+    const header = req.headers['x-ms-client-principal'];
+    const encoded = Buffer.from(header, 'base64');
+    const decoded = encoded.toString('ascii');
+
+    context.log("client principal: " + decoded)
+
     context.res = {
         headers: {
-          'Content-Type': 'application/json'    
+            'Content-Type': 'application/json'
         },
-        body: customers
+        // body: customers
+        body: JSON.parse(decoded)
     };
 
-// module.exports = async function (context, req) {
-    
-//     context.log('Header: ' + req.headers['authorization']);
-   
-    
-//     context.res.json({
-//         text: "Hello from the API"
-//     });
+    // module.exports = async function (context, req) {
+
+    //     context.log('Header: ' + req.headers['authorization']);
+
+
+    //     context.res.json({
+    //         text: "Hello from the API"
+    //     });
 };
